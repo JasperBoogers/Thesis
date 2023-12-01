aec98778e150abe5e2c69ee2901546f914888a2a
Jasper Boogers <48677568+JasperBoogers@users.noreply.github.com>
2023-12-01 12:45:54 +0100

    mwe using vtk

--- contourplot_vtk.py
+++ contourplot_vtk.py
@@ -16,6 +16,7 @@ from vtk import (
     vtkOrientationMarkerWidget,
     vtkNamedColors,
     vtkAxesActor,
+    vtkCubeSource,
     vtkCenterOfMass)
 import os
 from parameters import par
@@ -56,24 +57,42 @@ def rotate(model, x, y):
 
     return flt.GetOutput()
 
+
 def main():
     # load parameters
     FILENAME = par['Filepath']
     NUM_IT = par['Res angle']
     MAX_ANGLE = par['Max angle']
 
-    # set up vtk actor
+    # load stl file
     reader = vtkSTLReader()
     reader.SetFileName(FILENAME)
     reader.Update()
-    obj = reader.GetOutput()
+
+    tfm = vtkTransform()
+    tfm.Identity()
+
+    filt = vtkTransformPolyDataFilter()
+    filt.SetInputConnection(reader.GetOutputPort())
+    filt.SetTransform(tfm)
 
     mapper = vtkPolyDataMapper()
+    mapper.SetInputConnection(filt.GetOutputPort())
     actor = vtkActor()
     actor.SetMapper(mapper)
 
+    cube = vtkCubeSource()
+    cube.SetCenter(0, 0, 0)
+    cube.SetYLength(2)
+    cubemap = vtkPolyDataMapper()
+    cubemap.SetInputConnection(cube.GetOutputPort())
+    cubeActor = vtkActor()
+    cubeActor.SetMapper(cubemap)
+
     # plot
     ren = vtkRenderer()
+    ren.AddActor(actor)
+    ren.AddActor(cubeActor)
     renWin = vtkRenderWindow()
     renWin.AddRenderer(ren)
     renWin.SetWindowName('ReadSTL')
@@ -82,8 +101,6 @@ def main():
     iren = vtkRenderWindowInteractor()
     iren.SetRenderWindow(renWin)
 
-    ren.AddActor(actor)
-
     # add origin
     widget = vtkOrientationMarkerWidget()
     rgba = [0] * 4
@@ -91,37 +108,33 @@ def main():
     widget.SetOutlineColor(rgba[0], rgba[1], rgba[2])
     widget.SetOrientationMarker(vtkAxesActor())
     widget.SetInteractor(iren)
-    widget.SetViewport(0.0, 0.0, 0.4, 0.4)
     widget.SetEnabled(1)
-    widget.InteractiveOn()
 
     iren.Initialize()
-
-    # move obj to origin
-    obj = move_to_origin(obj)
     ren.ResetCamera()
+    ren.GetActiveCamera().Zoom(0.5)
 
     # perform rotations
     ax = ay = np.linspace(-MAX_ANGLE, MAX_ANGLE, NUM_IT)
     f = np.zeros((NUM_IT, NUM_IT))
 
-    mapper.SetInputData(obj)
-
     start = time.time()
     for i, x in enumerate(ax):
         for j, y in enumerate(ay):
-            # orient about x/y axis
-            obj_rot = rotate(obj, x, y)
-            mapper.SetInputData(obj_rot)
+            # create new transform instance
+            time.sleep(0.005)
+            tfm.Identity()
+            tfm.RotateX(x)
+            tfm.RotateY(y)
 
-            ren.ResetCamera()
+            print(tfm.GetOrientation())
+
+            # update pipeliine
             renWin.Render()
-            time.sleep(0.01)
-
-            f[i, j] = eval(obj_rot)
 
     end = time.time()
     print(f'execution duration: {end-start} seconds')
+    iren.Start()
 
  
 if __name__ == "__main__":
--- parameters.py
+++ parameters.py
@@ -4,6 +4,6 @@ import numpy as np
 
 par = {
     'Filepath': os.path.join('Geometries', '3DBenchy', '3DBenchy-bridge.stl'),
-    'Max angle': np.pi,
+    'Max angle': 90,
     'Res angle': 20
 }
