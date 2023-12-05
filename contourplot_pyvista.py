379b9c04d2280bb54967f85feb7748d7650c07c4
Jasper Boogers <48677568+JasperBoogers@users.noreply.github.com>
2023-12-05 14:48:51 +0100

    refactor, use vtkTransform

--- contourplot_pyvista.py
+++ contourplot_pyvista.py
@@ -3,6 +3,7 @@ import pyvista as pv
 from parameters import par
 import time
 from Mesh import Mesh
+from vtk import vtkTransform
 
 
 def eval_f(m, p):
@@ -16,35 +17,46 @@ def main():
     NUM_IT = par['Res angle']
     MAX_ANGLE = par['Max angle']
     ORIGIN = par['Origin']
-    proj_origin = [0, 0, -21]
+    proj_origin = [0, 0, -20]
 
-    mesh = Mesh(FILENAME)
-    proj = mesh.project_points_to_plane(origin=proj_origin)
-    _ = mesh.move_cog_to(ORIGIN)
-
-    # perform grid search using rotations
+    # create a pv Plotter and show origin
     plot = pv.Plotter()  # type: ignore
     plot.add_axes_at_origin()
+
+    # create a mesh, move the CoG to the origin and add to plotter
+    mesh = Mesh(FILENAME)
+    _ = mesh.move_cog_to(ORIGIN)
     plot.add_mesh(mesh.data, color='green', name='object')
+
+    # create a projection of the mesh on the xy plane,
+    # add to plotter and show plot
+    proj = mesh.project_points_to_plane(origin=proj_origin)
     plot.add_mesh(proj, color='blue', name='projection')
     plot.show(interactive_update=True)
 
+    # iterate over angles, and gather projected areas
     ax = ay = np.linspace(-MAX_ANGLE, MAX_ANGLE, NUM_IT)
     f = np.zeros((ax.shape[0], ay.shape[0]))
+    tfm = vtkTransform()
 
     start = time.time()
     for i, y in enumerate(ax):
         for j, x in enumerate(ay):
-            time.sleep(0.1)
+            time.sleep(0.001)
+            tfm.Identity()
+            tfm.RotateX(x)
+            tfm.RotateY(y)
+            mesh.transform(tfm)
 
-            mesh.rotate_x(x, abs=True)
-            mesh.rotate_y(y, abs=True)
+            # mesh.rotate_x(x, abs=True)
+            # mesh.rotate_y(y, abs=True)
             proj = mesh.project_points_to_plane(origin=proj_origin)
             plot.add_mesh(proj, color='blue', name='projection')
             plot.update()
 
             area = proj.area
             print(f'Area at x,y: {x, y}: {area}')
+            print(f'orientation: {tfm.GetOrientation()}')
             f[i, j] = area
 
     end = time.time()
