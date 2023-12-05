4e2e3fb678e87b164baea5bdd336a5cdc24a5d14
Jasper Boogers <48677568+JasperBoogers@users.noreply.github.com>
2023-12-05 10:00:27 +0100

    mess around with typesetting

--- contourplot_pyvista.py
+++ contourplot_pyvista.py
@@ -1,3 +1,4 @@
+from typing import Callable
 import numpy as np
 import pyvista as pv
 from parameters import par
@@ -6,62 +7,59 @@ import time
 
 class Mesh:
 
-    def __init__(self, file, x=0, y=0):
+    def __init__(self, file, x=0, y=0) -> None:
         self.data = pv.read(file)
         self.rot_x = x
         self.rot_y = y
 
-    def get_orientation(self):
+    def get_orientation(self) -> tuple:
         return self.rot_x, self.rot_y
 
-    def __getattr__(self, attr):
+    def __getattr__(self, attr) -> Callable:
         return getattr(self.data, attr)
 
-    def rotate_x(self, angle, point=(0, 0, 0), abs=False):
+    def rotate_x(self, angle, point=(0, 0, 0), abs=False) -> None:
         if abs:
             a = angle - self.rot_x
         else:
             a = angle
 
-        self.data.rotate_x(a, point=point, inplace=True)
+        self.data.rotate_x(a, point=point, inplace=True)  # type: ignore
         self.rot_x = self.rot_x + a
 
-    def rotate_y(self, angle, point=(0, 0, 0), abs=False):
+    def rotate_y(self, angle, point=(0, 0, 0), abs=False) -> None:
         if abs:
-            a = angle - self.rot_x
+            a = angle - self.rot_y
         else:
             a = angle
-        
-        self.data.rotate_y(a, point=point, inplace=True)
+
+        self.data.rotate_y(a, point=point, inplace=True)  # type: ignore
         self.rot_y = self.rot_y + a
 
-    def move_to_origin(self):
-        cog = self.data.center_of_mass()
-        self.data.translate(-cog, inplace=True)
+    def move_to_origin(self) -> None:
+        """Moves the center of mass to its origin
+        """
+        cog = self.data.center_of_mass()  # type: ignore
+        self.data.translate(-cog, inplace=True)  # type: ignore
 
 
-    # def set_rotation(self, x, y, inplace=True):
-    #     super().rotate_x(x - self.rot_x, inplace=inplace)
-    #     super().rotate_y(y - self.rot_y, inplace=inplace)
-    #     self.rot_x = x
-    #     self.rot_y = y
+def eval_f(m):
+    cog = m.data.center_of_mass()
+    return cog[-1]
 
 
 def main():
     # load parameters
     FILENAME = par['Filepath']
-    NUM_IT = par['Res angle']   
+    NUM_IT = par['Res angle']
     MAX_ANGLE = par['Max angle']
 
     file = pv.read(FILENAME)
     mesh = Mesh(FILENAME)
+    mesh.move_to_origin()
 
-    # add origin
-    ax = pv.Axes(show_actor=True)
-    ax.origin = (0, 0, 0)
-
-    plot = pv.Plotter()
-    plot.add_actor(ax.actor)
+    plot = pv.Plotter()  # type: ignore
+    plot.add_axes_at_origin()
     plot.add_mesh(mesh.data, color='green')
 
     plot.show(interactive_update=True)
@@ -71,20 +69,25 @@ def main():
     f = np.zeros((NUM_IT, NUM_IT))
 
     start = time.time()
-    time.sleep(2)
-    mesh.move_to_origin()
-    plot.update()
-    time.sleep(2)
-    mesh.rotate_x(90)
-    plot.update()
-    time.sleep(2)
-    mesh.rotate_x(-90)
-    plot.update()
-
-    plot.show()
+    for i, x in enumerate(ax):
+        for j, y in enumerate(ay):
+            time.sleep(2)
+            mesh.rotate_x(x, abs=True)
+            mesh.rotate_y(y, abs=True)
+            print(mesh.get_orientation())
+            plot.update()
+            f[i, j] = eval_f(mesh)
 
     end = time.time()
+    plot.show()
+
+    # # surface plot
+    # x, y = np.meshgrid(ax, ay)
+    # surface = pv.StructuredGrid(x, y, f)
+    # surface.plot(show_edges=True, show_grid=True)
+    
     print(f'execution duration: {end-start} seconds')
+
 
 if __name__ == "__main__":
     main()
