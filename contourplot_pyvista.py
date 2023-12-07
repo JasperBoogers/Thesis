a9adb9a1cf053880e429bd7d68ad2005cd6b0e17
Jasper Boogers <48677568+JasperBoogers@users.noreply.github.com>
2023-12-07 18:13:26 +0100

    use scipy Rotation instead of vtkTransform, reset orientation works

--- contourplot_pyvista.py
+++ contourplot_pyvista.py
@@ -3,11 +3,12 @@ import pyvista as pv
 from parameters import par
 import time
 from Mesh import Mesh
-from vtk import vtkTransform
+# from vtk import vtkTransform
+from scipy.spatial.transform import Rotation
 
 
-def eval_f(m, p):
-    proj = m.project_points_to_plane(origin=p)
+def eval_f(model: Mesh, plane):
+    proj = model.project_points_to_plane(origin=plane)
     return proj.area
 
 
@@ -37,16 +38,27 @@ def main():
     # iterate over angles, and gather projected areas
     ax = ay = np.linspace(-MAX_ANGLE, MAX_ANGLE, NUM_IT)
     f = np.zeros((ax.shape[0], ay.shape[0]))
-    tfm = vtkTransform()
+    rot = Rotation.from_matrix(np.identity(3))
+    tfm = np.zeros((4, 4))
+    tfm[:-1, :-1] = rot.as_matrix()
+    tfm[-1, -1] = 1
 
     start = time.time()
     for i, y in enumerate(ax):
         for j, x in enumerate(ay):
             time.sleep(0.01)
-            tfm.Identity()
-            tfm.RotateX(x)
-            tfm.RotateY(y)
+
+            # get inverse orientation and transform back to initial config
+            inv_or = rot.inv()
+            tfm[:-1, :-1] = inv_or.as_matrix()
             mesh.transform(tfm)
+
+            # now do a new rotation based on Euler angles
+            rot = Rotation.from_euler('XYZ', [x, y, 0], True)
+            tfm[:-1, :-1] = rot.as_matrix()
+            mesh.transform(tfm)
+
+            plot.update()
 
             proj = mesh.project_points_to_plane(origin=proj_origin)
             plot.add_mesh(proj, color='blue', name='projection')
@@ -54,7 +66,6 @@ def main():
 
             area = proj.area
             print(f'Area at x,y: {x, y}: {area}')
-            print(f'orientation: {tfm.GetOrientation()}')
             f[i, j] = area
 
     end = time.time()
