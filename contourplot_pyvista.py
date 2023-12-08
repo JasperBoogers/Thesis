3e6161c0257218cf96f5d2516932195618450cf8
Jasper Boogers <48677568+JasperBoogers@users.noreply.github.com>
2023-12-08 13:23:38 +0100

    generate output functions

--- contourplot_pyvista.py
+++ contourplot_pyvista.py
@@ -20,6 +20,9 @@ def main():
     ORIGIN = par['Origin']
     PROJ_OFFSET = -5
     REDUCTION = par['Resolution reduction']
+    OUTPATH = par['Outpath']
+    gif = False
+    vtp = False
 
     # create a pv Plotter and show origin
     plot = pv.Plotter()  # type: ignore
@@ -38,6 +41,8 @@ def main():
     proj = mesh.project_points_to_plane(origin=proj_origin)
     plot.add_mesh(proj, color='blue', name='projection')
     plot.show(interactive_update=True)
+    if gif:
+        plot.open_gif('benchy.gif')
 
     # iterate over angles, and gather projected areas
     ax = ay = np.linspace(-MAX_ANGLE, MAX_ANGLE, NUM_IT)
@@ -45,10 +50,12 @@ def main():
     rot = Rotation.from_matrix(np.identity(3))
     tfm = np.identity(4)
 
+    # parameters for output generation
+    meshes = []
+
     start = time.time()
     for i, x in enumerate(ax):
         for j, y in enumerate(ay):
-            # time.sleep(0.01)
 
             # get inverse orientation and transform back to initial config
             inv_or = rot.inv()
@@ -60,25 +67,56 @@ def main():
             tfm[:-1, :-1] = rot.as_matrix()
             mesh.transform(tfm)
 
-            plot.update()
-
+            # project the mesh to the z plane
             proj_origin = [0, 0, mesh.bounds[-2] + PROJ_OFFSET]  # type: ignore
             proj = mesh.project_points_to_plane(origin=proj_origin)
             plot.add_mesh(proj, color='blue', name='projection')
+
+            # update the plot and write to file
             plot.update()
+            meshes.append(mesh.data.copy())  # type: ignore
 
             area = proj.area
-            # print(f'Area at x,y: {x, y}: {area}')
             f[i, j] = area
+
+    if vtp:
+        for n, m in enumerate(meshes):
+            filename = OUTPATH + f'3DBenchy-{n}.vtp'
+            m.save(filename)
+
+    # retrieve the optimal orientation
+    opt_idx = np.unravel_index(np.argmin(f), f.shape)
+    x = ax[opt_idx[0]]
+    y = ay[opt_idx[1]]
+    print(f'optimal orientation at {x, y} degrees, f={f[opt_idx]}')
+    # get inverse orientation and transform back to initial config
+    inv_or = rot.inv()
+    tfm[:-1, :-1] = inv_or.as_matrix()
+    mesh.transform(tfm)
+
+    # now do a new rotation based on Euler angles
+    rot = Rotation.from_euler('XYZ', [x, y, 0], True)
+    tfm[:-1, :-1] = rot.as_matrix()
+    mesh.transform(tfm)
+
+    # project the mesh to the z plane
+    proj_origin = [0, 0, mesh.bounds[-2] + PROJ_OFFSET]  # type: ignore
+    proj = mesh.project_points_to_plane(origin=proj_origin)
+    plot.add_mesh(proj, color='blue', name='projection')
+    plot.update()
 
     end = time.time()
     print(f'execution duration: {end-start} seconds')
-    # plot.show()
 
     # surface plot
     x, y = np.meshgrid(ax, ay)
     surface = pv.StructuredGrid(x, y, f)
-    surface.plot(show_edges=True, show_grid=True)
+    surf_plot = pv.Plotter()  # type: ignore
+    surf_plot.add_mesh(surface, scalars=surface.points[:, -1], show_edges=True,
+                       scalar_bar_args={'vertical': True})
+    surf_plot.set_scale(zscale=0.1)
+    surf_plot.show_grid()
+    surf_plot.show()
 
 
 if __name__ == "__main__":
--- parameters.py
+++ parameters.py
@@ -5,7 +5,8 @@ import numpy as np
 par = {
     'Filepath': os.path.join('Geometries', '3DBenchy', '3DBenchy.stl'),
     'Max angle': 90,
-    'Res angle': 20,
+    'Res angle': 5,
     'Origin': np.array([0, 0, 0]),
-    'Resolution reduction': 0.9
+    'Resolution reduction': 0.9,
+    'Outpath': 'out/contourplot/3DBenchy/'
 }
