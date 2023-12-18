6cc102828b0c6b332de44242502a4c224434fd99
Jasper Boogers <48677568+JasperBoogers@users.noreply.github.com>
2023-12-18 11:24:47 +0100

    add support_volume_2D.py

--- support_volume_2D.py
+++ support_volume_2D.py
@@ -1,9 +1,12 @@
 # from scipy.spatial.transform import Rotation
 import numpy as np
 from scipy.optimize import minimize
+import time
+import jax.numpy as jnp
+from jax import grad, value_and_grad, jit
 
 
-def support_volume_2D(t, pts, proj):
+def support_2D_Euler(t, pts, proj):
     t = t[0]
     R = np.array([[np.cos(t), -np.sin(t)], [np.sin(t), np.cos(t)]])
     points_rot = R @ pts
@@ -21,16 +24,61 @@ def support_volume_2D(t, pts, proj):
     # calculate derivative
     dSdt = 2*proj*np.sin(t) - 2*proj*np.cos(t)
 
-    # set correct types for output
+    # set correct dtypes for output
     S = S[0]
     dSdt = np.array(dSdt)
     return -S, -dSdt
 
 
-points = np.array([[-1, 1, 1, -1], [-1, -1, 1, 1]])
-plane = -4  # projection plane y-coordinate
-t0 = [np.pi/10]
+@jit
+def support_2D_jax(t, pts, proj):
+    t = t[0]
+    R = jnp.array([[jnp.cos(t), -jnp.sin(t)], [jnp.sin(t), jnp.cos(t)]])
+    points_rot = R @ pts
 
-res = minimize(support_volume_2D, t0, args=(points, plane),
-               jac=True, options={'disp': True})
-print(res)
+    # extract points
+    p1, p2, p3, p4 = jnp.split(points_rot, 4, axis=1)
+
+    # calculate support volume
+    S1 = 0.5*(p1[0] - p4[0]) * (p4[1] - p1[1])
+    S2 = (p1[0] - p4[0]) * (p1[1] - proj)
+    S3 = (p2[0] - p1[0]) * (p1[1] - proj)
+    S4 = 0.5*(p2[0] - p1[0]) * (p2[1] - p1[1])
+    S = S1 + S2 + S3 + S4
+
+    # calculate derivative
+    # dSdt = 2*proj*np.sin(t) - 2*proj*np.cos(t)
+
+    # set correct dtypes for output
+    S = S[0]
+
+    return -S
+
+
+def support_2D_jax_grad(t, pts, plane):
+    return value_and_grad(support_2D_jax, 0)(t, pts, plane)
+
+
+if __name__ == "__main__":
+
+    # set mesh and projection plane
+    points = np.array([[-1, 1, 1, -1], [-1, -1, 1, 1]])
+    plane = -4
+
+    # initial conditions
+    t0 = [np.pi/10]
+
+    print('Solve using Euler angles')
+    start = time.time()
+    res_euler = minimize(support_2D_Euler, t0, args=(points, plane),
+                         jac=True, options={'disp': True})
+    t_euler = time.time() - start
+    print(res_euler)
+
+    print('Solve using JAX')
+    start = time.time()
+    res_jax = minimize(support_2D_jax_grad, t0, args=(points, plane),
+                       jac=True, options={'disp': True})
+    t_jax = time.time() - start
+    print(res_jax)
+    print(f'Analytic: {t_euler} seconds, Jax: {t_jax}')
