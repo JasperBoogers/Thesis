79abfe82e35ec1763ca4e9c5e0c252942db521f8
Jasper Boogers <48677568+JasperBoogers@users.noreply.github.com>
2023-12-19 13:09:21 +0100

    save cube.stl and adapt support_volume_3d

--- support_volume_3D.py
+++ support_volume_3D.py
@@ -10,29 +10,16 @@ from vtk import vtkTrimmedExtrusionFilter
 ORIGIN = np.array([0, 0, 0])
 OVERHANG_THRESHOLD = np.deg2rad(0)
 PROJ = -2
-ROTATION = [np.pi/4, np.pi/6, 0]
+ROTATION = [np.pi/4, 0, 0]
 
 # create a pv Plotter and show axis system
 plot = pv.Plotter()  # type: ignore
 plot.add_axes(shaft_length=0.9)
 plot.add_axes_at_origin()
 
-# define cube points in undeformed config
-x_coords = [-1, 1, 1, -1, -1, 1, 1, -1]
-y_coords = [-1, -1, 1, 1, -1, -1, 1, 1]
-z_coords = [-1, -1, -1, -1, 1, 1, 1, 1]
-points = np.array([x_coords, y_coords, z_coords])
-faces = np.hstack([[4, 0, 1, 2, 3],
-                  [4, 4, 5, 6, 7],
-                  [4, 1, 2, 6, 5],
-                  [4, 0, 3, 7, 4],
-                  [4, 0, 1, 5, 4],
-                  [4, 3, 2, 6, 7]])
-
 # create mesh
-mesh = pv.PolyData(points.transpose(), faces)
-mesh.compute_normals(inplace=True, flip_normals=True)
-area = mesh.compute_cell_sizes(length=False, volume=False)
+mesh = pv.read('Geometries/cube.stl')  # type: ignore
+mesh.compute_normals(inplace=True)  # type: ignore
 
 # rotate mesh and add to plot
 tfm = np.identity(4)
@@ -42,8 +29,8 @@ mesh.transform(tfm)
 plot.add_mesh(mesh, style='wireframe', color='green')
 plot.show(interactive_update=True)
 
-# calc support volume
-plane = pv.Plane(center=(0, 0, PROJ), i_size=5, j_size=5, direction=(0, 0, 1))
+# calc support volume, plane size should exceed model bounds! #TODO
+plane = pv.Plane(center=(0, 0, PROJ), i_size=10, j_size=10, direction=(0, 0, 1))
 
 overhang_idx = np.arange(mesh.n_cells)[mesh['Normals'][:, 2] < 0.0]
 overhang = mesh.extract_cells(overhang_idx)
