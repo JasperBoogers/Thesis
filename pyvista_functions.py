fix 2D derivatives

pyvista_functions.py
@@ -1,7 +1,7 @@
 import pyvista as pv
 import numpy as np
 from scipy.spatial.transform import Rotation
-
+from matplotlib import pyplot as plt
 
 def prep_mesh(m: pv.PolyData | pv.DataSet, decimation=0.9, flip=False) -> pv.PolyData:
     # ensure mesh is only triangles
@@ -75,3 +75,10 @@ def make_surface_plot(x: np.ndarray, y: np.ndarray, f: np.ndarray):
     surf_plot.set_scale(zscale=5)
     surf_plot.show_grid()
     surf_plot.show()
+
+def make_contour_plot(x: np.ndarray, y: np.ndarray, f: np.ndarray):
+    x, y = np.meshgrid(x, y)
+    fig = plt.figure()
+    cp = plt.contourf(x, y, f)
+    fig.colorbar(cp)
+    plt.show()

support_volume_2D.py
@@ -4,7 +4,16 @@ from scipy.optimize import minimize
 import time
 import jax.numpy as jnp
 from jax import value_and_grad, jit
-import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
+import matplotlib as mpl
+from latex_params import latex_params
+# mpl.rcParams.update(latex_params['params'])
+mpl.rcParams['text.usetex'] = False
+
+
+def finite_differences(y, x):
+    h = (x[-1] - x[0])/len(x)
+    return np.diff(y)/h
 
 
 def support_2D_Euler(t, points, faces, normals, proj):
@@ -20,7 +29,8 @@ def support_2D_Euler(t, points, faces, normals, proj):
     normals_rot = R @ np.transpose(normals)
 
     # determine the lowest point to project to
-    z_min = np.min(points_rot) - proj
+    z_min = np.min(points_rot[-1, :])
+    # points_rot[-1, :] -= z_min
 
     # identify downward facing normals
     overhang_idx = np.arange(faces.shape[0])[normals_rot[1, :] < -1e-6]
@@ -35,17 +45,23 @@ def support_2D_Euler(t, points, faces, normals, proj):
         p2 = points_rot[:, faces[idx][1]]
         dp2 = dpoints[:, faces[idx][1]]
 
-        # calculate A & h, multiply to get S
-        A = p2[0] - p1[0]
-        h = (p2[-1] + p1[-1])/2 - z_min
-        S += A*h
-
-        # calculate dA & dh to get dS
+        # calculate A & dA
         dA = dp2[0] - dp1[0]
-        dh = (dp2[-1] + dp1[-1])/2
+        A = p2[0] - p1[0]
+
+        # calculate h & dh, order of points matters!
+        if p2[-1] < p1[-1]:
+            h = (p1[-1] - p2[-1])/2
+            dh = (dp1[-1] - dp2[-1])/2
+        else:
+            h = (p2[-1] - p1[-1])/2
+            dh = (dp2[-1] - dp1[-1]) / 2
+
+        # multiply A, h, dA & dh to get S & dS
+        S += A * h
         dS += (dA * h + A * dh)
 
-    return -S, -dS
+    return S, dS
 
 
 if __name__ == "__main__":
@@ -56,7 +72,8 @@ if __name__ == "__main__":
     n = np.array([[0, -1], [1, 0], [0, 1], [-1, 0]])
     plane = 0
 
-    angles = np.linspace(-2*np.pi, 2*np.pi, 201)
+    step = 201
+    angles = np.linspace(-1*np.pi, 1*np.pi, step)
     support = np.zeros_like(angles)
     dSdt = np.zeros_like(angles)
     for k, angle in enumerate(angles):
@@ -65,33 +82,21 @@ if __name__ == "__main__":
         dSdt[k] = ds
 
     fig = plt.figure()
-    plt.plot(angles, 4*np.cos(angles)**2-2, 'b')
-    plt.plot(angles, np.sin(4*angles)/np.abs(np.cos(angles)*np.sin(angles)), 'r')
+    plt.plot(angles, support, 'g', label='Support')
+    plt.plot(angles, dSdt, 'b.', label=r"Calculated derivative")
+    plt.plot(angles[:-1], finite_differences(support, angles), 'r.', label='Finite difference')
+    # plt.plot(angles, np.sin(4*angles)/np.abs(np.cos(angles)*np.sin(angles)), 'r')
     plt.xlabel('Angle [rad]')
     plt.ylabel('Magnitude [-]')
-    plt.legend(['General solution', 'Specific solution'])
-    plt.ylim([-4, 4])
-    plt.xlim([0, np.pi])
-    plt.savefig('out/supportvolume/2D_solution_comp.svg')
-    plt.show()
-
-    fig2 = plt.figure()
-    plt.plot(angles, -support, 'b')
-    plt.plot(angles, np.sin(4 * angles) / np.abs(np.cos(angles) * np.sin(angles)), 'r')
-    plt.xlabel('Angle [rad]')
-    plt.ylabel('Magnitude [-]')
-    plt.legend(['Area', 'dA/dtheta'])
-    plt.ylim([-4, 4])
-    plt.xlim([0, np.pi])
-    plt.savefig('out/supportvolume/2D_specific.svg')
+    plt.legend()
+    plt.savefig('out/supportvolume/2D_solution_comp_fixed.svg', format='svg', bbox_inches='tight')
     plt.show()
 
     # initial conditions
     t0 = np.array([np.pi/10])
 
-    print('Solve using Euler angles')
     start = time.time()
-    res_euler = minimize(support_2D_Euler, t0, args=(p, f, n, plane),
+    res_euler = minimize(support_2D_Euler, t0, args=(p, f, n, plane), method='BFGS',
                          jac=True, options={'disp': True})
     t_euler = time.time() - start
     print(res_euler)