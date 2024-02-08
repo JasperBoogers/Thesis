c6b585b0552bfcc124b7dd08bc0400cc07bcb80e
Jasper Boogers <48677568+JasperBoogers@users.noreply.github.com>
2024-02-08 13:16:15 +0100

    Update support_volume_2D.py

--- support_volume_2D.py
+++ support_volume_2D.py
@@ -16,7 +16,7 @@ def finite_differences(y, x):
     return np.diff(y)/h
 
 
-def support_2D_Euler(t, points, faces, normals, proj):
+def support_2D(t, points, faces, normals, proj):
     t = t[0]
 
     # set up (derivative of) rotation matrix
@@ -29,8 +29,15 @@ def support_2D_Euler(t, points, faces, normals, proj):
     normals_rot = R @ np.transpose(normals)
 
     # determine the lowest point to project to
-    z_min = np.min(points_rot[-1, :])
-    # points_rot[-1, :] -= z_min
+    if proj > 0:
+        z_min = np.array([0, -proj])
+        dz_min = dR @ (np.transpose(R) @ z_min)
+    elif proj < 0:
+        z_min = points_rot[:, np.argmin(points_rot[-1, :])]
+        dz_min = dR @ (np.transpose(R) @ z_min)
+    else:
+        z_min = [0]
+        dz_min = [0]
 
     # identify downward facing normals
     overhang_idx = np.arange(faces.shape[0])[normals_rot[1, :] < -1e-6]
@@ -46,28 +53,32 @@ def support_2D_Euler(t, points, faces, normals, proj):
         dp2 = dpoints[:, faces[idx][1]]
 
         # calculate A & dA
-        dA = dp2[0] - dp1[0]
         A = p2[0] - p1[0]
+        dA = dp2[0] - dp1[0]
 
-        # calculate h & dh, order of points matters!
-        if p2[-1] < p1[-1]:
-            h = (p1[-1] - p2[-1])/2
-            dh = (dp1[-1] - dp2[-1])/2
+        if proj == 0:
+            # calculate h & dh, order of points matters!
+            if p2[-1] < p1[-1]:
+                h = (p1[-1] - p2[-1])/2
+                dh = (dp1[-1] - dp2[-1])/2
+            else:
+                h = (p2[-1] - p1[-1])/2
+                dh = (dp2[-1] - dp1[-1]) / 2
         else:
-            h = (p2[-1] - p1[-1])/2
-            dh = (dp2[-1] - dp1[-1]) / 2
+            h = (p2[1]+p1[1])/2 - z_min[-1]
+            dh = (dp2[-1] + dp1[-1]) / 2 - dz_min[-1]
 
         # multiply A, h, dA & dh to get S & dS
         S += A * h
         dS += (dA * h + A * dh)
 
-    return S, dS
+    return S, dS, z_min[-1]
 
 
 if __name__ == "__main__":
 
     # set mesh and projection plane
-    p = np.array([[-1, 1, 1, -1], [-1, -1, 1, 1]])
+    p = np.array([[-1/2, 1/2, 1/2, -1/2], [-1/2, -1/2, 1/2, 1/2]])
     f = np.array([[0, 1], [1, 2], [2, 3], [3, 0]])
     n = np.array([[0, -1], [1, 0], [0, 1], [-1, 0]])
     plane = 0
@@ -76,27 +87,31 @@ if __name__ == "__main__":
     angles = np.linspace(-1*np.pi, 1*np.pi, step)
     support = np.zeros_like(angles)
     dSdt = np.zeros_like(angles)
+    lowest_z = np.zeros_like(angles)
     for k, angle in enumerate(angles):
-        s, ds = support_2D_Euler([angle], p, f, n, plane)
+        s, ds, z = support_2D([angle], p, f, n, plane)
         support[k] = s
         dSdt[k] = ds
+        lowest_z[k] = z
 
     fig = plt.figure()
     plt.plot(angles, support, 'g', label='Support')
     plt.plot(angles, dSdt, 'b.', label=r"Calculated derivative")
     plt.plot(angles[:-1], finite_differences(support, angles), 'r.', label='Finite difference')
+    # plt.plot(angles, lowest_z, 'r', label='Lowest y-coordinate')
     # plt.plot(angles, np.sin(4*angles)/np.abs(np.cos(angles)*np.sin(angles)), 'r')
     plt.xlabel('Angle [rad]')
     plt.ylabel('Magnitude [-]')
-    plt.legend()
-    plt.savefig('out/supportvolume/2D_solution_comp_fixed.svg', format='svg', bbox_inches='tight')
+    plt.title('Rotating a square - no projection')
+    plt.legend(loc='upper right')
+    plt.savefig('out/supportvolume/2D_solution_no_proj.svg', format='svg', bbox_inches='tight')
     plt.show()
 
     # initial conditions
     t0 = np.array([np.pi/10])
 
     start = time.time()
-    res_euler = minimize(support_2D_Euler, t0, args=(p, f, n, plane), method='BFGS',
+    res_euler = minimize(support_2D, t0, args=(p, f, n, plane), method='BFGS',
                          jac=True, options={'disp': True})
     t_euler = time.time() - start
     print(res_euler)
