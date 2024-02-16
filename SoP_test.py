016f8e09bbc75f407d5a2cc0e9707ba54d888f88
Jasper Boogers <48677568+JasperBoogers@users.noreply.github.com>
2024-02-16 10:30:26 +0100

    Update SoP_test.py
    now also extract cells

--- SoP_test.py
+++ SoP_test.py
@@ -5,7 +5,7 @@ from helpers import *
 # load file and rotate
 FILE = 'Geometries/chair.stl'
 mesh = pv.read(FILE)
-mesh = prep_mesh(mesh.rotate_x(90, inplace=True))
+mesh = prep_mesh(mesh.rotate_x(45, inplace=True))
 
 # extract upward facing triangles
 thresh = 1e-6
@@ -14,7 +14,7 @@ upward = mesh.extract_cells(upward_idx)
 
 # compute average cell area and sampling cell length
 A_avg = upward.area/upward.n_cells
-L = np.sqrt(A_avg)/2
+L = np.sqrt(A_avg)/4
 bounds = upward.bounds
 x_coords = np.linspace(bounds[0] + L/2, bounds[1] - L/2, int((bounds[1] - bounds[0])/L))
 y_coords = np.linspace(bounds[2] + L/2, bounds[3] - L/2, int((bounds[3] - bounds[2])/L))
@@ -22,15 +22,40 @@ y_coords = np.linspace(bounds[2] + L/2, bounds[3] - L/2, int((bounds[3] - bounds
 z_min = 1.1*bounds[-2]
 z_max = 1.1*bounds[-1]
 
+top_idx = set()
 lines = []
+
 for x in x_coords:
     for y in y_coords:
+
+        # make a line
         line = pv.Line([x, y, z_min], [x, y, z_max])
         lines.append(line)
 
+        # check if any cells intersect that line
+        intersect = upward.find_cells_intersecting_line(line.points[0], line.points[1])
+
+        if len(intersect) > 1:
+
+            # calculate average center coordinate of each intersecting cell
+            points = np.array([upward.extract_cells(i).points for i in intersect])
+            centers = np.sum(points, axis=1)
+
+            # add cell idx with highest z-coordinate to top_idx
+            max_idx = np.argmax(centers[:, -1])
+            top_idx.add(intersect[max_idx])
+        elif len(intersect) > 0:
+            # only one intersecting cell -> top cover
+            top_idx.add(intersect[0])
+        else:
+            pass
+
+top_cover = upward.extract_cells(list(top_idx))
+
 # plot
 p = pv.Plotter()
-_ = p.add_mesh(upward, color='g', opacity=0.5)
+_ = p.add_mesh(upward, show_edges=True, color='g', opacity=0.5)
+_ = p.add_mesh(top_cover, show_edges=True, color='r', opacity=0.5)
 for line in lines:
     _ = p.add_mesh(line, color='b')
 p.show()
