dfe6da206b559e83eb48b7e59a8f373e33348585
Jasper Boogers <48677568+JasperBoogers@users.noreply.github.com>
2024-08-25 13:45:19 +0200

    Update support_volume_2D.py

--- support_volume_2D.py
+++ support_volume_2D.py
@@ -1,13 +1,4 @@
-import numpy as np
-from scipy.optimize import minimize
-import time
-from matplotlib import pyplot as plt
-import matplotlib as mpl
-from helpers import *
-from latex_params import latex_params
-
-# mpl.rcParams.update(latex_params['params'])
-mpl.rcParams['text.usetex'] = False
+from helpers.helpers import *
 
 
 def support_2D(t, points, faces, normals, proj):
@@ -39,7 +30,7 @@ def support_2D(t, points, faces, normals, proj):
     S = 0
     dS = 0
 
-    for idx in overhang_idx:
+    for idx in overhang_idx:  # TODO: vectorize
         # extract points corresponding to idx
         p1 = points_rot[:, faces[idx][0]]
         dp1 = dpoints[:, faces[idx][0]]
@@ -82,109 +73,78 @@ if __name__ == "__main__":
     support = np.zeros_like(angles)
     support_adap = np.zeros_like(angles)
 
-    support_fix = np.zeros_like(angles)
     dSdt = np.zeros_like(angles)
     dSdt_adap = np.zeros_like(angles)
-    dSdt_fix = np.zeros_like(angles)
     lowest_z = np.zeros_like(angles)
     for k, angle in enumerate(angles):
         s, ds, z = support_2D([angle], p, f, n, None)
-        # s_a, ds_a, _ = support_2D([angle], p, f, n, 0)
-        # s_f, ds_f, _ = support_2D([angle], p, f, n, 1)
+        s_a, ds_a, _ = support_2D([angle], p, f, n, 0)
 
         support[k] = s
-        # support_fix[k] = s_f
-        # support_adap[k] = s_a
+        support_adap[k] = s_a
 
         dSdt[k] = ds
-        # dSdt_fix[k] = ds_f
-        # dSdt_adap[k] = ds_a
+        dSdt_adap[k] = ds_a
 
         lowest_z[k] = z
 
     # substitute nan values
-    idx = np.where(np.abs(np.diff(dSdt)) >= 0.5)[0]
-    idx = np.append(idx, [idx-1, idx+1, idx+2])
-    pos = np.append(idx, [1, -2, -1])
+    nan_idx = np.where(np.abs(np.diff(dSdt)) >= 0.5)[0]
+    nan_idx = np.append(nan_idx, [nan_idx - 1, nan_idx + 1, nan_idx + 2])
+    pos = np.append(nan_idx, [1, -2, -1])
     x = np.insert(angles, pos, np.nan)
     dSdt = np.insert(dSdt, pos, np.nan)
 
-    #### plotting fixed projection
+    # plotting fixed projection
     fig, ax = plt.subplots(1, 1)
     ax.plot(np.rad2deg(angles), support, 'b', label='General solution')
-    ax.plot(np.rad2deg(angles), np.abs(np.sin(angles)*np.cos(angles)), 'r.', label='Specific solution')
+    ax.plot(np.rad2deg(angles), np.abs(np.sin(angles) * np.cos(angles)), 'r.', label='Specific solution')
     # ax.plot(np.rad2deg(angles), finite_central_differences(support, angles), 'r.', label='Finite differences')
     ax.set_xlabel(r'$\theta$ [deg]')
-    ax.set_ylabel(r'Area [mm$^2$]')
+    ax.set_ylabel(r'A [mm$^2$]')
     ax.legend()
     # fig.suptitle('Rotating a square, projection to y=-1')
     plt.savefig('out/supportvolume/2D/2D_solution_comp.svg', format='svg')
     plt.show()
 
-    fig, ax = plt.subplots(1, 1)
+    _, ax = plt.subplots(1, 1)
     ax.plot(np.rad2deg(x), dSdt, 'b', label='General solution')
-    ax.plot(np.rad2deg(angles), np.sin(4*angles)/2/np.abs(np.sin(2*angles)), 'r.', label='Specific solution')
-    # ax.plot(np.rad2deg(angles), finite_central_differences(support, angles), 'r.', label='Finite differences')
+    ax.plot(np.rad2deg(angles), np.sin(4 * angles) / 2 / np.abs(np.sin(2 * angles)), 'r.', label='Specific solution')
+    ax.plot(np.rad2deg(angles), finite_central_differences(support, angles), 'r.', label='Finite differences')
     ax.set_xlabel(r'$\theta$ [deg]')
-    ax.set_ylabel(r'Area derivative [mm$^2$/deg]')
+    ax.set_ylabel(r'A$_{,\theta}$ [mm$^2$/deg]')
     ax.legend()
     # fig.suptitle('Rotating a square, projection to y=-1')
     plt.savefig('out/supportvolume/2D/2D_derivative_comp.svg', format='svg')
     plt.show()
 
-    ##### comparison between general and specific solution, no projection #####
-    # fig, (ax1, ax2) = plt.subplots(2, 1)
-    # ax1.plot(np.rad2deg(angles), np.abs(np.sin(angles) * np.cos(angles)+1), 'r', label='Specific solution')
-    # ax1.plot(np.rad2deg(angles), support, 'b.', markersize=4, label='General solution')
-    # # ax1.set_xlabel('Angle [deg]')
-    # ax1.set_ylabel(r'Area [mm$^2$]')
-    # ax1.legend()
-    #
-    # # ax2.plot(np.rad2deg(angles), finite_central_differences(support, angles), 'r.', label='Finite differences')
-    # ax2.plot(np.rad2deg(angles), np.sin(4 * angles) / np.abs(2 * np.sin(2 * angles)), 'r', label='Specific solution')
-    # ax2.plot(np.rad2deg(angles), dSdt, 'b.', markersize=6, label=r"General solution")
-    # ax2.set_xlabel('Angle [deg]')
-    # ax2.set_ylabel(r'Area derivative [mm$^2$/deg]')
-    # ax2.legend()
-    #
-    # fig.suptitle('Area below unit square, comparison of general and specific solutions')
-    #
-    # plt.savefig('out/supportvolume/2D/2D_solution_comp_no_proj.svg', format='svg')
-    # fig.show()
+    # comparison between general and specific solution, no projection #
+    _, (ax1, ax2) = plt.subplots(2, 1)
+    ax1.plot(np.rad2deg(angles), np.abs(np.sin(angles) * np.cos(angles)+1), 'r', label='Specific solution')
+    ax1.plot(np.rad2deg(angles), support, 'b.', markersize=4, label='General solution')
+    # ax1.set_xlabel('Angle [deg]')
+    ax1.set_ylabel(r'Area [mm$^2$]')
+    ax1.legend()
 
-    # #### compare general solutions, no projection vs adaptive projection
-    # fig, ax = plt.subplots(1, 1)
-    # ax.plot(np.rad2deg(angles), support, 'r', label='No projection')
-    # ax.plot(np.rad2deg(angles), support_adap, 'b.', label='Projection to lowest point')
-    # ax.legend()
-    # ax.set_xlabel('Angle [deg]')
-    # ax.set_ylabel(r'Area [mm$^2$]')
-    # fig.suptitle('Solution comparison: no projection vs. projection to lowest point')
-    # plt.savefig('out/supportvolume/2D/2D_comp_no_proj_vs_adap_proj.svg', format='svg')
-    # fig.show()
-    #
-    # #### comparison of specific and general solution, projection to y=-1
-    # fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
-    # ax1.plot(np.rad2deg(angles), support, 'r', label='No projection')
-    # ax1.plot(np.rad2deg(angles), support_fix, 'g', label='Projection to y=-1')
-    # ax1.legend()
-    # ax1.set_ylabel(r'Area [mm$^2$]')
-    #
-    # ax2.plot(np.rad2deg(angles), dSdt, 'r', label='No projection')
-    # ax2.plot(np.rad2deg(angles), dSdt_fix, 'g', label='Projection to y=-1')
-    # ax2.set_ylabel(r'Area derivative [mm$^2$/deg]')
-    # ax2.set_xlabel('Angle [deg]')
-    # ax2.legend()
-    # fig.suptitle('Solution comparison: no projection vs. projection to y=-1')
-    # plt.savefig('out/supportvolume/2D/2D_comp_no_proj_vs_fix_proj.svg', format='svg')
-    # fig.show()
+    ax2.plot(np.rad2deg(angles), finite_central_differences(support, angles), 'k.', label='Finite differences')
+    ax2.plot(np.rad2deg(angles), np.sin(4 * angles) / np.abs(2 * np.sin(2 * angles)), 'r', label='Specific solution')
+    ax2.plot(np.rad2deg(angles), dSdt, 'b.', markersize=6, label=r"General solution")
+    ax2.set_xlabel('Angle [deg]')
+    ax2.set_ylabel(r'Area derivative [mm$^2$/deg]')
+    ax2.legend()
 
-    # initial conditions
-    # t0 = np.array([np.pi / 10])
+    fig.suptitle('Area below unit square, comparison of general and specific solutions')
 
-    # start = time.time()
-    # res_euler = minimize(support_2D, t0, args=(p, f, n, plane), method='BFGS',
-    #                      jac=True, options={'disp': True})
-    # t_euler = time.time() - start
-    # print(res_euler)
-    # print(f'Execution time: {t_euler}')
+    plt.savefig('out/supportvolume/2D/2D_solution_comp_no_proj.svg', format='svg')
+    fig.show()
+
+    # compare general solutions, no projection vs adaptive projection
+    fig, ax = plt.subplots(1, 1)
+    ax.plot(np.rad2deg(angles), support, 'r', label='No projection')
+    ax.plot(np.rad2deg(angles), support_adap, 'b.', label='Projection to lowest point')
+    ax.legend()
+    ax.set_xlabel('Angle [deg]')
+    ax.set_ylabel(r'Area [mm$^2$]')
+    fig.suptitle('Solution comparison: no projection vs. projection to lowest point')
+    plt.savefig('out/supportvolume/2D/2D_comp_no_proj_vs_adap_proj.svg', format='svg')
+    fig.show()
