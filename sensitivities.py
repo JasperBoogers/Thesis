910de8acb45f30ac9ae532f8265e9227afe02eb8
Jasper Boogers <48677568+JasperBoogers@users.noreply.github.com>
2024-08-25 13:11:06 +0200

    Update sensitivities.py

--- sensitivities.py
+++ sensitivities.py
@@ -1,13 +1,8 @@
 import time
-import numpy as np
-import pyvista as pv
-import matplotlib.pyplot as plt
-from helpers import *
-from io_helpers import read_connectivity_csv
-from support_volume_3D import support_volume_smooth, support_volume_analytic
-from support_volume_2D import support_2D
+from helpers.helpers import *
 from SoP import SoP_connectivity
 from joblib import delayed, Parallel
+from os import cpu_count
 
 
 def fwd(fun, angles, mesh, args, h):
@@ -74,7 +69,6 @@ def finite_differences_plot(fun, angles, mesh, args, h_range, method='forward', 
     _ = plt.loglog(h_range, diffy, 'k', label=r'$V_{,\beta}$')
     # plt.title(title)
     plt.xlabel('Step size [-]')
-    # plt.ylabel(r'$\frac{|\tilde{V}_{,\theta} - V_{,\theta}|}{|V_{,\theta}|}$')
     plt.ylabel('Relative error [-]')
     _ = plt.legend()
     if outfile is not None:
@@ -126,92 +120,35 @@ def plot_cell_sensitivities(mesh: pv.PolyData | pv.DataSet, axis: str = 'x') -> 
     p.show()
 
 
-def func(angles, m, arg):
-    a, b = angles
-    v = a ** 2 * b ** 2
-    dvda = 2 * a * b ** 2
-    dvdb = 2 * a ** 2 * b
-    return v, [dvda, dvdb]
-
-
-def func32(angles, m, arg):
-    a, b = angles
-    v = a ** 2 * b ** 2
-    dvda = 2 * a * b ** 2
-    dvdb = 2 * a ** 2 * b
-    return np.float32(v), np.float32([dvda, dvdb])
-
-
 if __name__ == '__main__':
     start = time.time()
-    #
-    # load file
-    # FILE = 'Geometries/bunny/bunny_coarse.stl'
-    FILE = 'Geometries/jet_bracket_2.stl'
-    m = pv.read(FILE)
-    # m = m.subdivide(2, subfilter='linear')
-    m = prep_mesh(m, decimation=0.8)
-    connectivity = read_connectivity_csv('out/sim_data/jet_bracket_2_conn.csv')
 
-    assert len(connectivity) == m.n_cells
+    FILE = 'Geometries/cube_cutout.stl'
+    geometry = pv.read(FILE)
+    geometry = geometry.subdivide(2, subfilter='linear')
+    geometry = prep_mesh(geometry, decimation=0)
+
+    # set parameters
+    print('Generating connectivity')
+    connectivity = read_connectivity_csv('out/sim_data/connectivity2.csv')
+    print(f'Connectivity took {time.time() - start} seconds')
+
     args = {
         'connectivity': connectivity,
         'build_dir': np.array([0, 0, 1]),
         'down_thresh': np.sin(np.deg2rad(45)),
         'up_thresh': np.sin(np.deg2rad(0)),
         'down_k': 10,
-        'up_k': 10,
-        'SoP_penalty': 1,
-        'softmin_p': -15
+        'up_k': 20,
+        'SoP_penalty': 0,
+        'softmin_p': -140
     }
 
-    m = calc_cell_sensitivities(m, np.deg2rad([481.92160597, 179.79582416]), args)
-    m.plot(scalars='V', scalar_bar_args={"title": "Support volume"}, cmap='RdYlBu', clim=[-1, 1],
-           show_edges=True, lighting=False)
-    steps = np.logspace(-10, 0, 10)
+    steps = np.logspace(-10, 0, 15)
 
     x = np.deg2rad([30, -30])
-    finite_differences_plot(SoP_connectivity, x, m, args, steps, 'forward'
-                            , 'out/supportvolume/SoP/finite_forward_differences_5010_normalized.svg')
+    finite_differences_plot(SoP_connectivity, x, geometry, args, steps, 'forward'
+                            , 'out/supportvolume/SoP/finite_forward_differences_3030_normalized.svg')
 
-    # FILE = 'Geometries/cube.stl'
-    # m = pv.read(FILE)
-    # m = prep_mesh(m, decimation=0)
-    # m = m.subdivide(2, subfilter='linear')
-    #
-    # # # set parameters
-    # OVERHANG_THRESHOLD = -1e-5
-    # PLANE_OFFSET = calc_min_projection_distance(m)
-    # args = [OVERHANG_THRESHOLD, PLANE_OFFSET]
-    #
-    # # x = np.deg2rad([10, 10])
-    # x = [1, 1]
-    # #
-    # steps = np.logspace(-10, 0, 10)
-    #
-    # fx = []
-    # fx2 = []
-    # # f, dfdx, _ = support_2D(x, points, faces, normals, plane)
-    # f, [dfdx, _] = func32(x, m, args)
-    # f2, [dfdx2, _] = func(x, m, args)
-    # for h in steps:
-    #     v, w = x
-    #     fhx, [_, _] = func32([v+h, w], m, args)
-    #     fhx2, [_, _] = func([v+h, w], m, args)
-    #     # fhx, _, _ = support_2D([x[0] + h], points, faces, normals, plane)
-    #     dx = (fhx - f)/h
-    #     dx2 = (fhx2 - f2)/h
-    #
-    #     fx.append((dx - dfdx)/dfdx)
-    #     fx2.append((dx2 - dfdx2)/dfdx2)
-    #
-    # plt.loglog(steps, abs(np.array(fx2)), label='double precision')
-    # plt.loglog(steps, abs(np.array(fx)), label='single precision')
-    # plt.xlabel('Step size [-]')
-    # plt.ylabel('Relative error [-]')
-    # plt.legend()
-    # plt.title(r'Relative error for $f=x^{2}y^{2}$ at [1, 1]')
-    # plt.savefig('out/precision_error.svg', format='svg', bbox_inches='tight')
-    # plt.show()
-    # stop = time.time()
-    # print(f'Time taken: {stop - start} seconds')
+    stop = time.time()
+    print(f'Time taken: {stop - start} seconds')
