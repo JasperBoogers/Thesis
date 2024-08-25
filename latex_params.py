130d77887ceeb8ff22600bd4048514eebfa2fc37
Jasper Boogers <48677568+JasperBoogers@users.noreply.github.com>
2024-08-25 12:20:53 +0200

    remove old files

--- latex_params.py
+++
@@ -1,9 +0,0 @@
-latex_params = {
-    'params': {
-        'text.usetex': False,
-        # 'pgf.rcfonts': False,
-        'font.family': 'arial',
-        # 'text.latex.unicode': True},
-    },
-    'textwidth': 6.202
-}
